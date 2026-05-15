ea8a28ef8170032b6630741b15e4c80280c96a40
teofils1 <teodor.marinescu03@stud.fils.upb.ro>
2026-05-15 22:36:12 +0300

    Initialize Spring Boot Maven project

---
+++ src/main/java/SeWeb_hw2/SeWebHw2Application.java
@@ -0,0 +1,13 @@
+package SeWeb_hw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeWebHw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeWebHw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/SeWeb_hw2/SeWebHw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package SeWeb_hw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeWebHw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
